06cf68e08183e73533fa8a39ee5561d309112b66
Rabiaa123 <r.amarire@gmail.com>
2025-02-14 11:15:16 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,6 +5,12 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)     
 
+@app.route('/somme/<int:valeur1>/<int:valeur2>')
+def somme(valeur1, valeur2):
+    resultat = valeur1 + valeur2
+    pair_ou_impair = "pair" if resultat % 2 == 0 else "impair"
+    return f"<h2>La somme de {valeur1} et {valeur2} est : {resultat} ({pair_ou_impair})</h2>"
+  
 @app.route('/calcul_carre/<int:val_user>')
 def carre(val_user):
     return "<h2>Le carré de votre valeur est : </h2>" + str(val_user * val_user)
