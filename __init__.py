c1ac147f7a1632f977e652c02d698c3d20b0b168
Rabiaa123 <r.amarire@gmail.com>
2025-02-14 13:05:54 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,33 +8,6 @@ app = Flask(__name__)
 @app.route('/calcul_carre/<int:val_user>')
 def carre(val_user):
     return "<h2>Le carré de votre valeur est : </h2>" + str(val_user * val_user)
-
-@app.route('/somme', methods=['GET', 'POST'])
-def somme():
-    if request.method == 'POST':
-        valeurs = request.form.get('valeurs')
-        try:
-            # Convertir la saisie en une liste d'entiers
-            nombres = list(map(int, valeurs.split(',')))
-            
-            # Calculer la somme avec une boucle
-            total = 0
-            for nombre in nombres:
-                total += nombre
-            
-            return f"<h2>La somme des valeurs {nombres} est : {total}</h2>"
-        except ValueError:
-            return "<h2>Erreur : Veuillez entrer uniquement des nombres séparés par des virgules.</h2>"
-
-    # Afficher le formulaire
-    return '''
-        <form method="post">
-            <label>Entrez des nombres séparés par des virgules :</label>
-            <input type="text" name="valeurs">
-            <button type="submit">Calculer</button>
-        </form>
-    '''
-
   
 @app.route("/contact/")
 def MaPremiereAPI():
