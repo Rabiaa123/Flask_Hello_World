b41c654c7edda0e93e8449942e5bfcc0980627cc
Rabiaa123 <r.amarire@gmail.com>
2025-02-14 13:47:15 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,22 +13,14 @@ def MaPremiereAPI1():
 def MaPremiereAPI():
     return render_template("contact.html")
 
-@app.route("/", methods=["GET", "POST"])
-def max_value():
-    max_val = None  # Initialisation de la valeur maximale
-    numbers = []
+@app.route('/')
+def hello_world():
+    return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
 
-    if request.method == "POST":
-        user_input = request.form["numbers"]
-        try:
-            numbers = list(map(float, user_input.split(",")))  # Convertir les valeurs
-            if numbers:
-                max_val = max(numbers)  # Trouver la valeur max
-        except ValueError:
-            return "Erreur : Veuillez entrer uniquement des nombres valides, séparés par des virgules."
-
-    return render_template("index.html", max_val=max_val, numbers=numbers)
-
+@app.route('/calcul_carre/<int:val_user>')
+def carre(val_user):
+    return f"<h2>Le carré de votre valeur est :</h2> {escape(val_user * val_user)}"
+  
 @app.route('/exercices/')
 def exercices():
     return render_template('exercices.html') #Commentaire
