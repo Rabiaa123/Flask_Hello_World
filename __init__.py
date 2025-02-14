d76ea5d9605a111701a1e2eb2e946f0cd89a56ed
Rabiaa123 <r.amarire@gmail.com>
2025-02-14 14:07:29 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -17,6 +17,12 @@ def MaPremiereAPI():
 def hello_world():
     return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
 
+@app.route('/somme_pair_impaire/<int:valeur1>/<int:valeur2>')
+def somme(valeur1, valeur2):
+    resultat = valeur1 + valeur2
+    pair_ou_impair = "pair" if resultat % 2 == 0 else "impair"
+    return f"<h2>La somme de {valeur1} et {valeur2} est : {resultat} ({pair_ou_impair})</h2>"
+
 @app.route('/calcul_carre/<int:val_user>')
 def carre(val_user):
     return "<h2>Le carré de votre valeur est : </h2>" + str(val_user * val_user)
