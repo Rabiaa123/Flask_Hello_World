9f1a4b4ce87c52b62ee773fa47b2da19449aa6ae
Rabiaa123 <r.amarire@gmail.com>
2025-02-14 10:14:53 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,8 +3,12 @@ from flask import render_template
 from flask import json
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+app = Flask(__name__)     
+
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
+  
 @app.route('/')
 def hello_world():
     return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
