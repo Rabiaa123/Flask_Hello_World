d0d362cda0352d1a9a34ce176d82a530949beee2
Rabiaa123 <r.amarire@gmail.com>
2025-02-26 09:23:29 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,10 +9,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
- 
+
 @app.route('/exercices/')
 def exercices():
-    return render_template('exercices.html')
+    return render_template('exercices.html') 
+
+@app.route('/exercice_base1/')
+def exercice1():
+    return render_template('1_Liste_Base.html')
 
 @app.route("/contact/")
 def MaPremiereAPI():
