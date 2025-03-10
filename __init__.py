0c1bcdee577941f879901310bed3660360582d8d
Rabiaa123 <r.amarire@gmail.com>
2025-03-10 10:05:22 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -77,7 +77,7 @@ def carre():
 
 @app.route('/etoiles/')
 def etoiles():
-    return render_template('carre_etoiles.html')
+    return render_template('Carre_Etoiles.html')
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
